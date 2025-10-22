ee3d0c46ca6ab2737ea94fe12ca7441a8b5a349a
Lê Anh Đức <leanhduc350@gmail.com>
2025-10-22 02:45:55 +0000

    Update Baitap1_Tuan6.c

--- Tuan 6/Baitap1_Tuan6.c
+++ Tuan 6/Baitap1_Tuan6.c
@@ -11,6 +11,7 @@ unsigned char seg7[10] =  {
   0x7F, // '8'
   0x6F  // '9'
 };
+unsigned char digit[4] = { 0, 0 ,0 ,0 };
 
 void IO_init(void)
 {
@@ -51,10 +52,33 @@ uint16_t ADC_read(void)
 
 void LED_init(unsigned int value)
 {
-	voltage = value * 5 / 1023 ;
+ char i;
+	voltage = value * 5 / 1023;
+
+ digit[3] = voltage/10/10/10%10;
+ digit[2] = voltage/10/10%10;
+ digit[1] = voltage/10%10;
+ digit[0] = voltage%10!
+
+
+ for(i = 0; i < 4; i++)
+ {
+   PORTA = (unsigned char)(0x80 >> i);
+   PORTD = digit[i];
+   delay_us(20);
+ }
+ 
+   
 
 
 }
 void main() {
+  IO_init();
+  ADC_init();
+  while(1)
+  {
+   value = ADC_read();
+   LED_init(value);
+  }
 
 }
